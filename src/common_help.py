9bf7afa9d750688555b9eeba93a44cdab8b08476
Kiki0327 <nicolasgil1406@gmail.com>
2025-08-26 18:37:35 -0500

    Check the function to change the name file

--- src/common_help.py
+++ src/common_help.py
@@ -1,6 +1,16 @@
+# Library imports
+
 import os
 
-dict_paths = {'payments':'G:/Mi unidad/Documentos/Finanzas/Pagos/'}
+#################################################################################################################################
+
+# Dictionary with paths
+
+dict_paths = {'payments':'G:\Mi unidad\Documentos\Finanzas\Pagos'}
+
+#################################################################################################################################
+
+# Functions
 
 def change_name(directory=str, key=str):
     """
@@ -18,4 +28,16 @@ def change_name(directory=str, key=str):
 
     # List all files in the specified directory
     dirs = os.listdir(os.path.join(directory, key))
+    print(f"Directory to change names: {os.path.join(directory, key)}")
     print(f"Count of files in directory: {len(dirs)} \nFirst 5 files: {dirs[:5]}")
+
+    # Iterate positions and change names
+    for file in range(len(dirs)):
+        # save extension
+        ext = dirs[file].split(".")[-1]
+        # Separating by . or -
+        separar = dirs[file].split(".")[0].split("-")
+        new_name = f"{separar[2]}{separar[1]}{separar[0]}.{ext}"
+        # print(f"\nOld name: {dirs[file]} \nNew name: {new_name}")
+        # Rename the file
+        os.rename(os.path.join(directory, key, dirs[file]), os.path.join(directory, key, new_name))
