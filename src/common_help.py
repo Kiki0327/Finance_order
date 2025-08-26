3957621f6b0fa847729a87100d2574db23ff8b48
Kiki0327 <nicolasgil1406@gmail.com>
2025-08-25 21:06:32 -0500

    Creando función

--- src/common_help.py
+++ src/common_help.py
@@ -1,2 +1,21 @@
-def hello():
-    return "Hello, world!"
+import os
+
+dict_paths = {'payments':'G:/Mi unidad/Documentos/Finanzas/Pagos/'}
+
+def change_name(directory=str, key=str):
+    """
+    Function to change the name of a file in the payments directory.
+
+    Arguments:
+        directory (str): The current name of the file to be changed.
+        key (str): The key corresponding to the directory in dict_paths.
+    
+    Returns:
+        None
+    """
+
+    print(f"Current directory: {directory}")
+
+    # List all files in the specified directory
+    dirs = os.listdir(os.path.join(directory, key))
+    print(f"Count of files in directory: {len(dirs)} \nFirst 5 files: {dirs[:5]}")
