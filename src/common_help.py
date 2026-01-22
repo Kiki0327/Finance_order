9d9bb373466cc61a475c17023262757726e99442
Kiki0327 <nicolasgil1406@gmail.com>
2026-01-21 20:10:12 -0500

    prueba

--- src/common_help.py
+++ src/common_help.py
@@ -141,3 +141,6 @@ def join_pdfs(pdf_paths: list[str], output_path: str):
     # Write the combined PDF to the specified output path
     with open(output_path, "wb") as output_pdf:
         writer.write(output_pdf)
+
+def new_fun ():
+    pass
